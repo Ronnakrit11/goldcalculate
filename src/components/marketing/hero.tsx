d1676ea644ad6cc1c3aaddbd525dacd6165aeeb8
nookteanook111 <vnfpower.finance@gmail.com>
2025-04-17 06:59:15 +0700

    done

--- src/components/marketing/hero.tsx
+++ src/components/marketing/hero.tsx
@@ -227,7 +227,7 @@ const Hero = () => {
                         <div className="flex items-center justify-center mt-6 gap-x-4">
                             <Link href="https://web.facebook.com/profile.php?id=61570053279894" className="flex items-center gap-2 group text-black">
                                 <Button size="lg">
-                                    ติดต่อเเอดมิน
+                                สนใจซื้อขาย คลิก
                                     <ArrowRightIcon className="size-4 group-hover:translate-x-1 transition-all duration-300" />
                                 </Button>
                             </Link>
