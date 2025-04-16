75ae81ccc67b52672140f9d77bc97552402bf88b
nookteanook111 <vnfpower.finance@gmail.com>
2025-04-16 21:59:55 +0700

    done

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,12 +1,5 @@
 export const NAV_LINKS = [
-    {
-        name: "Pricing",
-        href: "#pricing"
-    },
-    {
-        name: "Features",
-        href: "#features"
-    },
+
     {
         name: "Blog",
         href: "#blog"
